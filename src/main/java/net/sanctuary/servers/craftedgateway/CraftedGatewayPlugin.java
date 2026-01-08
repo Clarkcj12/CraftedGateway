452007bf4420343d2b0bc641709daac3f2d51f29
Clarkcj <clarkcj01@gmail.com>
2026-01-07 23:28:40 -0500

    Add ACF command framework bootstrap

--- src/main/java/net/sanctuary/servers/craftedgateway/CraftedGatewayPlugin.java
+++ src/main/java/net/sanctuary/servers/craftedgateway/CraftedGatewayPlugin.java
@@ -1,15 +1,22 @@
 package net.sanctuary.servers.craftedgateway;
 
+import co.aikar.commands.BukkitCommandManager;
+import net.sanctuary.servers.craftedgateway.command.GatewayCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CraftedGatewayPlugin extends JavaPlugin {
+    private BukkitCommandManager commandManager;
+
     @Override
     public void onEnable() {
+        commandManager = new BukkitCommandManager(this);
+        commandManager.registerCommand(new GatewayCommand(this));
         getLogger().info("CraftedGateway enabled.");
     }
 
     @Override
     public void onDisable() {
+        commandManager = null;
         getLogger().info("CraftedGateway disabled.");
     }
 }
